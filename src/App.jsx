2c125da2f771603db0bcbaf9e2c23a71d85b9cdf
Jenesyx <a.bidkhori2004@gmail.com>
2024-12-31 14:15:16 +0100

    Added pricing section and improved the heading section and added some tagline for recommend use

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Button from './components/Button'
 import Collabration from './components/Collabration'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Pricing from './components/Pricing'
+import Services from './components/Services'
 
 const App = () => {
 
@@ -14,6 +16,8 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collabration />
+        <Services />
+        <Pricing />
       </div>
         <ButtonGradient />
     </>
